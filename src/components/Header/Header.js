81e920ef62deefbd39b7294c3df4607cf2577442
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-11 15:54:10 +0500

    style main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import style from './header.module.css'
 import logo from './../../img/logo.png'
 import search from './../../img/search.png'
+import client from './../../img/client.png'
 
 function Header(){
     return(
@@ -16,6 +17,40 @@ function Header(){
                     <a className={style.menu_in}>Sale</a>
                 </div>
                 <img className={style.search} src={search}/>
+            </div>
+            <div className={style.main}>
+                <div className={style.information}>
+                    <h1>Get your awesome sneakers.</h1>
+                    <h2>We offer the best deals in our shop. Check them out now. 
+                        We have awesome stuff on sale for you.
+                    </h2>
+                    <div className={style.button}>
+                        <button className={style.btn_main}>Shop Now</button>
+                        <div className={style.heart}></div>
+                    </div>
+                    <div className={style.check}>
+                        <div className={style.check_in}>
+                            <input type="checkbox"/>
+                            <h3>Free shipping</h3>
+                        </div>
+                        <div className={style.check_in}>
+                            <input type="checkbox"/>
+                            <h3>Free returns</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.client}>
+                    <div className={style.client_in}>
+                        <img src={client}/>
+                        <h1 className="client_title">
+                            <span className={style.client_title_main}>Joan</span>
+                            <span className={style.client_title_secondary}> 
+                                has just purchased
+                                these sneakers now.
+                            </span>
+                        </h1>
+                    </div>
+                </div>
             </div>
         </div>
         </>
