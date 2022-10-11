b000a896cb0166aafe7afd058430461d9ca9287c
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-11 19:28:41 +0500

    style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Accesories from './components/Accesories/Accesories';
 import Features from './components/Features/Features';
 import Header from './components/Header/Header';
 
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <Header/>
       <Features/>
+      <Accesories/>
     </div>
   );
 }
---
+++ src/components/Accesories/Accesories.js
@@ -0,0 +1,41 @@
+import React from "react";
+import style from './accesories.module.css'
+import football from './../../img/football.png'
+import volley from './../../img/volley.png'
+
+function Accesories(){
+    return(
+        <>
+        <div className={style.cantainer}>
+            <div className={style.accesories_football}>
+                <div className={style.accesories_in_football}>
+                    <img src={football}/>
+                    <div className={style.football}>
+                        <h4 className={style.title_football}>Accesories</h4>
+                        <h3>Football</h3>
+                        <h4 className={style.subtitle}>We receive new sportwear every day. 
+                            Just take your pick.
+                        </h4>
+                        <button>Shop Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className={style.volley}>
+                <div className={style.volley}>
+                    <img src={volley}/>
+                    <div className={style.volley}>
+                        <h4 className={style.title_volley}>Accesories</h4>
+                        <h3>Volleyball</h3>
+                        <h4 className={style.subtitle}>We receive new sportwear every day. 
+                            Just take your pick.
+                        </h4>
+                        <button>Shop Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Accesories
