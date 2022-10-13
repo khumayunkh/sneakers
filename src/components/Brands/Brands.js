2fbcf0471e8da9c84944fb600646fe1263dc82d5
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-13 16:43:31 +0500

    brands

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -8,7 +8,34 @@ import brand from './../../img/brand.png'
 function Brands(){
     return(
         <>
-
+        <div className={style.container}>
+            <div className={style.brands}>
+                <div className={style.back}></div>
+                <h2 className={style.title}>Popular brands</h2>
+                <div className={style.brands_in}>
+                    <div className={style.brand}>
+                        <div className={style.brand_in}>
+                            <img src={nike}/>
+                        </div>
+                    </div>
+                    <div className={style.brand}>
+                        <div className={style.brand_in}>
+                            <img src={newBalance}/>
+                        </div>
+                    </div>
+                    <div className={style.brand}>
+                        <div className={style.brand_in}>
+                            <img src={adidas}/>
+                        </div>
+                    </div>
+                    <div className={style.brand}>
+                        <div className={style.brand_in}>
+                            <img src={brand}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
