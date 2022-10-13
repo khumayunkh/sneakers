1338afc16e7e1502dd88f12ad2d1cc05ed1a3a0d
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-13 17:37:35 +0500

    subscribe

--- src/components/Subscribe/Subscribe.js
+++ src/components/Subscribe/Subscribe.js
@@ -8,14 +8,16 @@ function Subscribe(){
         <div className={style.container}>
             <div className={style.back}></div>
             <div className={style.subscribe}>
-                <h2>Subscribe now to get the latest updates</h2>
                 <div className={style.subscribe_in}>
+                <h2>Subscribe now to get the latest updates</h2>
+                <div className={style.subscribe_email}>
                     <div className={style.email}>
                         <input className={style.input} placeholder="Enter you email"/>
                         <button>SUBSCRIBE</button>
                     </div>
-                    <div className={style.back1}></div>
                     <img src={sneakers}/>
+                    <div className={style.back1}></div>
+                </div>
                 </div>
             </div>
         </div>
